43226c6b3cad1eee5a9d1a0fdc20b6568153ca41
koki-develop <kou.pg.0131@gmail.com>
2023-03-19 07:30:11 +0900

    support svg-min format

---
+++ pkg/formatter/svg_minified.go
@@ -0,0 +1,43 @@
+package formatter
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/alecthomas/chroma/v2"
+	"github.com/alecthomas/chroma/v2/formatters"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/svg"
+)
+
+var (
+	SVGMinified = formatters.Register("svg-min", NewSVGMinifiedFormatter())
+)
+
+type SVGMinifiedFormatter struct {
+	svgFormatter chroma.Formatter
+	m            *minify.M
+}
+
+func NewSVGMinifiedFormatter() *SVGMinifiedFormatter {
+	m := minify.New()
+	m.AddFunc("image/svg+xml", svg.Minify)
+
+	return &SVGMinifiedFormatter{
+		svgFormatter: formatters.SVG,
+		m:            m,
+	}
+}
+
+func (f *SVGMinifiedFormatter) Format(w io.Writer, style *chroma.Style, iterator chroma.Iterator) error {
+	b := new(bytes.Buffer)
+	if err := f.svgFormatter.Format(b, style, iterator); err != nil {
+		return err
+	}
+
+	if err := f.m.Minify("image/svg+xml", w, b); err != nil {
+		return err
+	}
+
+	return nil
+}
