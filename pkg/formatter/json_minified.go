201d28469fc13a0261207d09652b19f37054cefe
koki-develop <kou.pg.0131@gmail.com>
2023-03-19 07:26:35 +0900

    support json-min format

---
+++ pkg/formatter/json_minified.go
@@ -0,0 +1,43 @@
+package formatter
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/alecthomas/chroma/v2"
+	"github.com/alecthomas/chroma/v2/formatters"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/json"
+)
+
+var (
+	JSONMinified = formatters.Register("json-min", NewJSONMinifiedFormatter())
+)
+
+type JSONMinifiedFormatter struct {
+	jsonFormatter chroma.Formatter
+	m             *minify.M
+}
+
+func NewJSONMinifiedFormatter() *JSONMinifiedFormatter {
+	m := minify.New()
+	m.AddFunc("application/json", json.Minify)
+
+	return &JSONMinifiedFormatter{
+		jsonFormatter: formatters.JSON,
+		m:             m,
+	}
+}
+
+func (f *JSONMinifiedFormatter) Format(w io.Writer, style *chroma.Style, iterator chroma.Iterator) error {
+	b := new(bytes.Buffer)
+	if err := f.jsonFormatter.Format(b, style, iterator); err != nil {
+		return err
+	}
+
+	if err := f.m.Minify("application/json", w, b); err != nil {
+		return err
+	}
+
+	return nil
+}
