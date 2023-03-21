b5b6e15437e5d53da14ac841bc42c44e5f429c2e
koki-develop <kou.pg.0131@gmail.com>
2023-03-21 13:37:19 +0900

    add css prettier

---
+++ pkg/prettier/css.go
@@ -0,0 +1,30 @@
+package prettier
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/client9/csstool"
+)
+
+var (
+	CSS = Register("CSS", NewCSSPrettier())
+)
+
+type CSSPrettier struct {
+}
+
+func NewCSSPrettier() *CSSPrettier {
+	return &CSSPrettier{}
+}
+
+func (p *CSSPrettier) Pretty(c string) (string, error) {
+	f := csstool.NewCSSFormat(2, false, nil)
+
+	b := new(bytes.Buffer)
+	if err := f.Format(strings.NewReader(c), b); err != nil {
+		return "", err
+	}
+
+	return b.String(), nil
+}
