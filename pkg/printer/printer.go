c16dbf999b89b46ed17f5acf94e2250a97b8dcfc
koki-develop <kou.pg.0131@gmail.com>
2023-03-21 12:16:47 +0900

    import style pkg

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -14,6 +14,7 @@ import (
 	"github.com/alecthomas/chroma/v2/styles"
 	_ "github.com/koki-develop/gat/pkg/formatter"
 	"github.com/koki-develop/gat/pkg/prettier"
+	_ "github.com/koki-develop/gat/pkg/style"
 )
 
 var (
