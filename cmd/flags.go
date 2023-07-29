893e921f0c45a6eec44d93fc9b7cb101601ef58e
koki-develop <kou.pg.0131@gmail.com>
2023-07-29 21:11:10 +0900

    Add environment variable support in flags.go

--- cmd/flags.go
+++ cmd/flags.go
@@ -1,23 +1,46 @@
 package cmd
 
-var (
-	flagLang   string
-	flagFormat string
-	flagTheme  string
+import "os"
 
+func envOrDefault(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+var (
+	// --lang
+	flagLang string
+
+	// --format
+	flagFormat        string
+	flagFormatDefault = envOrDefault("GAT_FORMAT", "terminal256")
+
+	// --theme
+	flagTheme        string
+	flagThemeDefault = envOrDefault("GAT_THEME", "monokai")
+
+	// --force-color
+	flagForceColor bool
+
+	// --pretty
 	flagPretty bool
 
-	flagListLangs   bool
-	flagListFormats bool
-	flagListThemes  bool
+	// --list-langs
+	flagListLangs bool
 
-	flagForceColor bool
+	// --list-formats
+	flagListFormats bool
+
+	// --list-themes
+	flagListThemes bool
 )
 
 func init() {
 	rootCmd.Flags().StringVarP(&flagLang, "lang", "l", "", "language for syntax highlighting")
-	rootCmd.Flags().StringVarP(&flagFormat, "format", "f", "terminal256", "output format")
-	rootCmd.Flags().StringVarP(&flagTheme, "theme", "t", "monokai", "highlight theme")
+	rootCmd.Flags().StringVarP(&flagFormat, "format", "f", flagFormatDefault, "output format")
+	rootCmd.Flags().StringVarP(&flagTheme, "theme", "t", flagThemeDefault, "highlight theme")
 	rootCmd.Flags().BoolVarP(&flagForceColor, "force-color", "c", false, "force colored output")
 
 	rootCmd.Flags().BoolVarP(&flagPretty, "pretty", "p", false, "whether to format a content pretty")
