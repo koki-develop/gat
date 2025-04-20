13bd2b06220be83dfb0aa03ca7adf34d5cc57cbf
koki-develop <kou.pg.0131@gmail.com>
2025-04-20 17:15:27 +0900

    chore: Split PrintFormats to printFormats and listFormats

--- internal/gat/formats.go
+++ internal/gat/formats.go
@@ -7,8 +7,19 @@ import (
 	"github.com/koki-develop/gat/internal/formatters"
 )
 
-func PrintFormats(w io.Writer) {
-	for _, f := range formatters.List() {
-		fmt.Fprintln(w, f)
+func PrintFormats(w io.Writer) error {
+	return printFormats(w, listFormats())
+}
+
+func printFormats(w io.Writer, formats []string) error {
+	for _, f := range formats {
+		if _, err := fmt.Fprintln(w, f); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func listFormats() []string {
+	return formatters.List()
 }
