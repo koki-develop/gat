2848dfd37e48f45e05f78007eee1e704f37212ba
koki-develop <kou.pg.0131@gmail.com>
2025-04-20 16:48:50 +0900

    chore: Split PrintLanguages to printLanguages and listLanguages

--- internal/gat/languages.go
+++ internal/gat/languages.go
@@ -9,15 +9,35 @@ import (
 	"github.com/koki-develop/gat/internal/lexers"
 )
 
+type Language struct {
+	Name    string
+	Aliases []string
+}
+
 func PrintLanguages(w io.Writer) error {
+	return printLanguages(w, listLanguages())
+}
+
+func printLanguages(w io.Writer, langs []Language) error {
 	tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', 0)
 
-	for _, l := range lexers.List() {
-		cfg := l.Config()
-		if _, err := tw.Write([]byte(fmt.Sprintf("%s\t%s\n", cfg.Name, strings.Join(cfg.Aliases, ", ")))); err != nil {
+	for _, l := range langs {
+		if _, err := tw.Write([]byte(fmt.Sprintf("%s\t%s\n", l.Name, strings.Join(l.Aliases, ", ")))); err != nil {
 			return err
 		}
 	}
 
 	return tw.Flush()
+}
+
+func listLanguages() []Language {
+	ls := lexers.List()
+
+	rtn := make([]Language, len(ls))
+	for i, l := range ls {
+		cfg := l.Config()
+		rtn[i] = Language{Name: cfg.Name, Aliases: cfg.Aliases}
+	}
+
+	return rtn
 }
